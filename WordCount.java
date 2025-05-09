ab8e63fba9875bea311eb24369cb3c0ceaf4599d
Saikrishna <pendemsaikrishna02@gmail.com>
2025-05-09 12:15:46 +0530

    Update WordCount.java

--- WordCount.java
+++ WordCount.java
@@ -1,6 +1,4 @@
 package string;
-
-
 public class WordCount {
 	public static void string (String s) {
 		String s1[]=s.split(" ");
@@ -9,18 +7,9 @@ public class WordCount {
 				System.out.println(s1[i]);
 			}
 		}
-		
 	public static void main(String [] args) {
 		String s="I am a Indian";
-
 			string(s);
-	
-//		String s1[]=s.split(" ");
-//		for(int i=0;i<=s1.length-1;i++) {
-//		if(i%2!=0) 
-//		System.out.println(s1[i]);
-		
-			
 		}
 
 	}
