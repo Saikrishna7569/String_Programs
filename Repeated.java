ca8c10a0c721632343ce1ef06b3ca1eb5bc189d0
Saikrishna <pendemsaikrishna02@gmail.com>
2025-05-09 12:12:13 +0530

    Update Repeated.java

--- Repeated.java
+++ Repeated.java
@@ -11,17 +11,8 @@ public class Repeated {
 			}
 		}
 		System.out.println(s2);
-
 	}
 	public static void main(String [] args) {
-	String s="java";
-//	String s2="";
-//	for(int i=0;i<=s.length()-1;i++) {
-//		char ch=s.charAt(i);
-//		if(s2.indexOf(ch)==-1) {
-	
-	
          string(s);
-	
 	}
 }
