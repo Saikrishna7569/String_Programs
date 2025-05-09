48b638e21d3a4d11675c9ebdd68cafc246820542
Saikrishna <pendemsaikrishna02@gmail.com>
2025-05-09 12:16:50 +0530

    Update stringUpper.java

--- stringUpper.java
+++ stringUpper.java
@@ -1,24 +1,4 @@
-//package string;
-//
-//public class stringUpper {
-//
-//	public static void main(String [] args) {
-//		String s="IndiA";
-//		for(int i=0;i<=s.length()-1;i++) {
-//			 char ch=s.charAt(i);
-////			 if(ch>65 && ch<90) 
-//			 //or
-//				 if(ch>='A' && ch<='Z'){
-//				 System.out.println(ch);
-//			 }
-//		}
-//	}
-//}
-
-
-////Write a program to print the char which are present in even indexes
 package string;
-
 public class stringUpper 
 {
 	public static boolean isEmpty(String s) {
@@ -37,17 +17,8 @@ public class stringUpper
 	}
 public static void main(String[] args) {
 	String s="Iamanindian";
-//	for (int i=0;i<=s.length()-1;i++) {
-//		if(i!=0 && i%2==0) {
-//			char ind=s.charAt(i);
-//
-//			System.out.println(ind);
 	 string(s);
-	 
-
 		}
-	
-
 }
 
 
